b1ef804b07dedf240b4c046f934fb93b2f9fb964
R.A. Stern <rastern@users.noreply.github.com>
2018-01-05 17:11:15 -0500

    Update documentation

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. vmt-util documentation master file, created by
+   sphinx-quickstart on Tue Jul 18 10:49:54 2017.
+
+.. # Links
+.. _API 2.0: https://cdn.turbonomic.com/wp-content/uploads/docs/VMT_REST2_API_PRINT.pdf
+.. _vmt-connect: https://rastern.github.io/vmt-connect/
+.. _vmt-plan: https://rastern.github.io/vmt-plan/
+
+vmt-util: Turbonomic Utilities
+=================================
+
+vmt-util is a single file Python module that is intended to be used in conjunction
+with the `vmt-connect`_ and `vmt-plan`_ modules, and provides utility methods
+used with the Turbonomic `API 2.0`_.
+
+
+.. toctree::
+   :maxdepth: 3
+
+   install
+   quickstart
+   vmtutil
--- vmtutil/vmtutil.py
+++ vmtutil/vmtutil.py
@@ -63,10 +63,6 @@ def evaluate(exp, local_map={}):
 
     Returns:
         Expression result.
-
-
-    Example:
-        ``evaluate(exp='getnum() < 5', local_map=('getnum': random.random)`` returns True if getnum() is less than 5, else False.
     """
     tree = VMTSafeEval().visit(ast.parse(exp, mode='eval'))
     ast.fix_missing_locations(tree)
@@ -98,7 +94,7 @@ def unit_cast(value, ufrom, uto, factor, unit_list):
 
 
 def mem_cast(value, unit='GB', factor=1024, src_unit='KB'):
-    """Converts memory values using the binary (1024) system of units.
+    """Converts values using the binary (1024) system of units.
 
     ``mem_cast`` is a wrapper for ``unit_cast`` and supports memory sizes from
     Bytes (B) to Yottabytes (YB). Source values are expected to be supplied as
@@ -125,7 +121,7 @@ def mem_cast(value, unit='GB', factor=1024, src_unit='KB'):
 
 
 def cpu_cast(value, unit='GHZ', factor=1000, src_unit='MZH'):
-    """Converts memory values using the SI (1000) system of units.
+    """Converts values using the SI (1000) system of units.
 
     ``cpu_cast`` is a wrapper for ``unit_cast`` and supports cpu speeds from
     Hertz (HZ) to Petahertz (PHZ). Source values are expected to be supplied as
